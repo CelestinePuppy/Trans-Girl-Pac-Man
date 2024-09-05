c520bc4dba31d1988c3d97b71c21af34d61270d6
Celestine <Sfcicak@gmail.com>
2024-09-04 20:56:35 -0400

    finished drawing the edges of the map and updated map.cs
    made some functions to make sure that the map edges are actually flush and unpassable type stuff.

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -13,11 +13,43 @@ public class Map : MonoBehaviour
     void Start()
     {
         mapArray = new bool[width, height];
+
+        BuildMapArrayFromTiles();
+        PrintMapArray();    
+    }
+    void BuildMapArrayFromTiles()
+    {
+        Transform[] children = GetComponentsInChildren<Transform>();
+
+        foreach (Transform child in children)
+        {
+            int x = Mathf.RoundToInt(child.position.x);
+            int y = Mathf.RoundToInt(child.position.y);
+            Debug.Log(child.position + " " + x + "," + y);
+            child.position = new Vector2(x, y);
+            mapArray[x, y] = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void PrintMapArray()
     {
-        
+        for (int i = 0; i < mapArray.GetLength(0); i++)
+        {
+            string row = "";
+
+            for (int j = 0; j < mapArray.GetLength(1); j++)
+            {
+                bool here = mapArray[i, j];
+                if (here)
+                {
+                    row += "1";
+                }
+                else
+                {
+                    row = "0";
+                }
+            }
+            Debug.Log(row);
+        }
     }
 }
